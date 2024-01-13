7548c68c2c7adaa7bd3d8f17e3390b6c6f9467ad
unknown <cufbas@gmail.com>
2024-01-13 18:57:16 +0100

    disabled shuffle

--- dtu_proj/models/make_embeddings.py
+++ dtu_proj/models/make_embeddings.py
@@ -3,14 +3,17 @@ if __name__ == '__main__':
     import torch.nn as nn
     import pandas as pd
     import glob
-
+    from sklearn.model_selection import train_test_split
     from model import RecommenderNet
     
-    user_rating = pd.read_csv('data/processed/clean.csv')
+    # load the data
+    data = pd.read_csv('data/processed/clean.csv')
+    # split the data
+    train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False, random_state=42)
 
     # get amount of unique books and users
-    n_books = user_rating['Book_Id'].unique().sum()
-    n_users = user_rating['ID'].unique().sum()
+    n_books = train_data['Book_Id'].unique().sum()
+    n_users = train_data['ID'].unique().sum()
 
     # Get the data and process it, use gpu if available
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
--- dtu_proj/models/train_model.py
+++ dtu_proj/models/train_model.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
         def __len__(self):
             return len(self.user_rating)
 
-
         def __getitem__(self, idx):
             user = torch.tensor(self.user_rating.iloc[idx]['ID'])
             book = torch.tensor(self.user_rating.iloc[idx]['Book_Id'])
@@ -52,13 +51,21 @@ if __name__ == '__main__':
             print(f'Epoch {epoch+1}/{epochs}, Loss: {loss.item()}')
 
 
-# load previously calculated embeddings
-model = torch.load("models/embeddings.pt")
-
 # load the data
 data = pd.read_csv('data/processed/clean.csv')
 # split the data
-train_data, test_data = train_test_split(data, test_size=0.2)
+train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False, random_state=42)
+
+# get amount of unique books and users
+n_books = train_data['Book_Id'].unique().sum()
+n_users = train_data['ID'].unique().sum()
+
+print(n_books)
+print(n_users)
+
+# load previously calculated embeddings
+model = RecommenderNet(n_users, n_books, n_factors=2)
+moedel= model.load_state_dict(torch.load("models/embeddings.pt"))
 
 # train the model
-train(train, data)
+train(model, train_data)
